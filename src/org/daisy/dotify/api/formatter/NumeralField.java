b9e7498e8f4fdf0361aeacc8f6ced0292eda87da
Joel Håkansson <joel.hakansson@mtm.se>
2013-11-12 13:29:11 +0000

    moved numeral formatting from API to formatter impl.

--- src/org/daisy/dotify/api/formatter/NumeralField.java
+++ src/org/daisy/dotify/api/formatter/NumeralField.java
@@ -23,31 +23,6 @@ public class NumeralField implements Field {
 		 * Defines alpha numeral style
 		 */
 		ALPHA, UPPER_ALPHA, LOWER_ALPHA;
-
-		/**
-		 * Formats the numeral with the given style
-		 * 
-		 * @param i
-		 *            the number
-		 * @return returns the formatted number
-		 */
-		public String format(int i) {
-			switch (this) {
-				case ROMAN:
-				case UPPER_ROMAN:
-					return RomanNumeral.int2roman(i);
-				case LOWER_ROMAN:
-					return RomanNumeral.int2roman(i).toLowerCase();
-				case ALPHA:
-				case UPPER_ALPHA:
-					return AlphaNumeral.int2alpha(i);
-				case LOWER_ALPHA:
-					return AlphaNumeral.int2alpha(i).toLowerCase();
-				case DEFAULT:
-				default:
-					return "" + i;
-			}
-		}
 	};
 
 	private NumeralStyle style;
