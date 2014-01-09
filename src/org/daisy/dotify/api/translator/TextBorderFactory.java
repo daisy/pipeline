98bee082f04f3bdf3b6cfcfc201b15fd833fc521
Joel Håkansson <joel.hakansson@mtm.se>
2014-01-09 10:36:37 +0000

    Removed old stuff in BrailleTextBorderFactory (continued) and TextBorderFactory

--- src/org/daisy/dotify/api/translator/TextBorderFactory.java
+++ src/org/daisy/dotify/api/translator/TextBorderFactory.java
@@ -7,16 +7,6 @@ public interface TextBorderFactory {
 	 * a string.
 	 */
 	public final static String FEATURE_MODE = "mode";
-	/**
-	 * Style feature. The corresponding value should be
-	 * Set<String> with style values.
-	 */
-	public final static String FEATURE_STYLE = "style";
-	public final static String STYLE_SOLID = "solid";
-	public final static String STYLE_WIDE = "wide";
-	public final static String STYLE_THIN = "thin";
-	public final static String STYLE_INNER = "inner";
-	public final static String STYLE_OUTER = "outer";
 
 	public void setFeature(String key, Object value);
 
