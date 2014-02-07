d6161aade8c2c0890d37f356e0f0cd62cd595abf
Joel Håkansson <joel.hakansson@mtm.se>
2014-02-07 08:31:28 +0000

    Cleaned up page struct interfaces

--- src/org/daisy/dotify/api/formatter/Page.java
+++ src/org/daisy/dotify/api/formatter/Page.java
@@ -8,43 +8,12 @@ import java.util.List;
  * @author Joel Håkansson
  */
 public interface Page {
-	
-	/**
-	 * Gets all markers for this page
-	 * @return returns a list of all markers on a page
-	 */
-	//public List<Marker> getMarkers();
-	
-	/**
-	 * Gets markers for this page excluding markers before text content
-	 * @return returns a list of markers on a page
-	 */
-	//public List<Marker> getContentMarkers();
-	
+
 	/**
 	 * Gets the rows on this page
 	 * @return returns the rows on this page
 	 */
 	public List<Row> getRows();
 
-	/**
-	 * Gets the page index for this page (zero based, but including page number offset)
-	 * @return returns the page index in the sequence
-	 */
-	public int getPageIndex();
-	
-	/**
-	 * Returns true if this page allows volume break after it.
-	 * @return returns true if the page allows volume break, false otherwise
-	 */
-	public boolean allowsVolumeBreak();
-	
-	public int keepPreviousSheets();
-
-	/**
-	 * Gets the parent sequence for this page
-	 * @return returns the parent sequence
-	 */
-	public PageSequence getParent();
 
 }
--- src/org/daisy/dotify/api/formatter/PageSequence.java
+++ src/org/daisy/dotify/api/formatter/PageSequence.java
@@ -20,21 +20,5 @@ public interface PageSequence{
 	 * @return returns the layout master for this sequence
 	 */
 	public LayoutMaster getLayoutMaster();
-	
-	/**
-	 * Gets the page with the specified index, where index >= 0 && index < getPageCount()
-	 * @param index the page index
-	 * @return returns the page index
-	 * @throws IndexOutOfBoundsException if index < 0 || index >= getPageCount()
-	 */
-	//public Page getPage(int index);
-	/**
-	 * Gets the page number offset for this page sequence
-	 * @return returns the page number offset
-	 */
-	//public int getPageNumberOffset();
-	
-	//public FormatterFactory getFormatterFactory();
-	
-	//public Formatter getFormatter();
+
 }
