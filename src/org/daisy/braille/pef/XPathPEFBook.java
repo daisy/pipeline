5f971d712e440796d546f4399f5a765d02d719c4
Joel Håkansson <joel.hakansson@mtm.se>
2014-11-21 17:48:01 +0000

    Moved DocumentBuilder based loading of PEFBook into separate class (XPathPEFBook)

---
+++ src/org/daisy/braille/pef/XPathPEFBook.java
@@ -0,0 +1,202 @@
+package org.daisy.braille.pef;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+class XPathPEFBook {
+	private static final Pattern eightDotPattern = Pattern.compile("[\u2840-\u28ff]");
+	
+	public static PEFBook load(URI uri) throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
+		return load(uri, false);
+	}
+
+	private static PEFBook load(URI uri, boolean continueOnError) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+		HashMap<String, List<String>> metadata;
+		// Book properties
+		int volumes;
+		int pageTags;
+		int pages;
+		int maxWidth;
+		int maxHeight;
+		String inputEncoding;
+		boolean containsEightDot;
+		int[] startPages;
+		int tmp = 0;
+		Document d = null;
+		String encoding = null;
+		metadata = new HashMap<String, List<String>>();
+		
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			dbf.setNamespaceAware(true);
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			d = db.parse(uri.toString());
+			encoding = d.getInputEncoding();
+			List<String> al;
+			NodeList nl = d.getDocumentElement().getElementsByTagName("meta").item(0).getChildNodes();
+			for (int i=0; i<nl.getLength(); i++) {
+				Node n = nl.item(i);
+				if (n!=null && n.getNodeType()==Node.ELEMENT_NODE) {
+					if ("http://purl.org/dc/elements/1.1/".equals(n.getNamespaceURI())) { 
+						String name = n.getLocalName();
+						if (metadata.containsKey(name)) {
+							al = metadata.remove(name);
+						} else {
+							al = new ArrayList<String>();
+						}
+						al.add(n.getTextContent());
+						metadata.put(name, al);
+					}
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			if (continueOnError) {
+				e.printStackTrace();
+			} else {
+				throw e;
+			}
+		} catch (SAXException e) {
+			if (continueOnError) {
+				e.printStackTrace();
+			} else {
+				throw e;
+			}
+		} catch (IOException e) {
+			if (continueOnError) {
+				e.printStackTrace();
+			} else {
+				throw e;
+			}
+		}
+		
+		inputEncoding = encoding;
+
+		XPath xp = XPathFactory.newInstance().newXPath();
+		xp.setNamespaceContext(new PEFNamespaceContext());
+
+		// Count volumes
+		tmp = 0;
+		try {
+			tmp = ((Double)xp.evaluate("count(//pef:volume)", d, XPathConstants.NUMBER)).intValue();
+		} catch (XPathExpressionException e) {
+			tmp = 0;
+		}
+		volumes = tmp;
+		
+		// Count page tags
+		tmp = 0;
+		try {
+			tmp = ((Double)xp.evaluate("count(//pef:page)", d, XPathConstants.NUMBER)).intValue();
+		} catch (XPathExpressionException e) {
+			if (continueOnError) {
+				tmp = 0;
+			} else {
+				throw e;
+			}
+		}
+		pageTags = tmp;
+
+		// Count pages including blank
+		tmp = 0;
+		try {
+			tmp = ((Double)xp.evaluate(
+				"count(//pef:section[ancestor-or-self::pef:*[@duplex][1][@duplex='false']]/descendant::pef:page)*2 + count(//pef:section[ancestor-or-self::pef:*[@duplex][1][@duplex='true']]/descendant::pef:page) + count(//pef:section[count(descendant::pef:page) mod 2 = 1][ancestor-or-self::pef:*[@duplex][1][@duplex='true']])-count(((//pef:section)[last()])[count(descendant::pef:page) mod 2 = 1][ancestor-or-self::pef:*[@duplex][1][@duplex='true']])", d, XPathConstants.NUMBER)).intValue();
+		} catch (XPathExpressionException e) {
+			if (continueOnError) {
+				tmp = 0;
+			} else {
+				throw e;
+			}
+		}
+		pages = tmp;
+		
+		// Get max width
+		tmp = 0;
+		try {
+			NodeList ns = (NodeList)xp.evaluate("//pef:*/@cols", d, XPathConstants.NODESET);
+			for (int i = 0; i < ns.getLength(); ++i) {
+				Attr attr = (Attr)ns.item(i);
+				String colsValue = attr.getNodeValue();
+				tmp = Math.max(tmp, Integer.valueOf(colsValue));				
+			}
+		} catch (XPathExpressionException e) {
+			if (continueOnError) {
+				tmp = 0;
+			} else {
+				throw e;
+			}
+		}
+		maxWidth = tmp;
+		
+		// Get max height
+		tmp = 0;
+		try {
+			NodeList ns = (NodeList)xp.evaluate("//pef:*/@rows", d, XPathConstants.NODESET);
+			for (int i = 0; i < ns.getLength(); ++i) {
+				Attr attr = (Attr)ns.item(i);
+				String colsValue = attr.getNodeValue();
+				tmp = Math.max(tmp, Integer.valueOf(colsValue));				
+			}
+		} catch (XPathExpressionException e) {
+			if (continueOnError) {
+				tmp = 0;
+			} else {
+				throw e;
+			}
+		}
+		maxHeight = tmp;
+		
+		// Contains eight dot?
+		boolean bTmp = false;
+		try {
+			NodeList texts = (NodeList)xp.evaluate("//pef:row/text()", d, XPathConstants.NODESET);
+			for (int i = 0; i < texts.getLength(); ++i) {
+				String text = texts.item(i).getTextContent();
+				if (eightDotPattern.matcher(text).find()) {
+					bTmp = true;
+				}
+			}
+		} catch (XPathExpressionException e) {
+			if (!continueOnError) {
+				throw e;
+			}
+		}
+		containsEightDot = bTmp;
+		
+		// get start pages
+		startPages = new int[volumes];
+		for (int i = 1; i <= volumes; i++) {
+			try {
+				Node page = (Node)xp.evaluate("(//pef:volume)[position()="+(i)+"]/descendant::pef:page[1]", d, XPathConstants.NODE);
+				int pageOffset = ((Double)xp.evaluate("count(preceding::pef:section[ancestor-or-self::pef:*[@duplex][1][@duplex='false']]/descendant::pef:page)*2 + count(preceding::pef:section[ancestor-or-self::pef:*[@duplex][1][@duplex='true']]/descendant::pef:page) + count(preceding::pef:section[count(descendant::pef:page) mod 2 = 1][ancestor-or-self::pef:*[@duplex][1][@duplex='true']])", page, XPathConstants.NUMBER)).intValue();
+				startPages[i-1] = pageOffset + 1;
+			} catch (XPathExpressionException e) { 
+				if (continueOnError) {
+					e.printStackTrace();
+					startPages[i-1] = 0;
+				} else {
+					throw e;
+				}
+			}
+		}
+		return new PEFBook(metadata, volumes, pages, pageTags, maxWidth, maxHeight, inputEncoding, containsEightDot, startPages);
+	}
+}
--- test/org/daisy/braille/pef/XPathPEFBookTest.java
+++ test/org/daisy/braille/pef/XPathPEFBookTest.java
@@ -1,14 +1,8 @@
 package org.daisy.braille.pef;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.URISyntaxException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,30 +11,11 @@ import javax.xml.xpath.XPathExpressionException;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-public class PEFBookTest {
-
-	@Test
-	public void testSerializable() throws IOException, XPathExpressionException, ParserConfigurationException, SAXException, URISyntaxException, ClassNotFoundException {
-		File f = File.createTempFile("PEFBookTest", ".tmp");
-		try {
-			PEFBook p1 = PEFBook.load(this.getClass().getResource("resource-files/PEFBookTestInput.pef").toURI());
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
-			oos.writeObject(p1);
-			oos.close();
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
-			PEFBook p2 = (PEFBook)ois.readObject();
-			ois.close();
-			assertTrue("Assert that deserialized object is equal to original object", p1.equals(p2));
-		} finally {
-			if (!f.delete()) {
-				f.deleteOnExit();
-			}
-		}
-	}
+public class XPathPEFBookTest {
 
 	@Test
 	public void testUnevenSectionsDuplex() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, URISyntaxException {
-		PEFBook p1 = PEFBook.load(this.getClass().getResource("resource-files/uneven-sections-duplex.pef").toURI());
+		PEFBook p1 = XPathPEFBook.load(this.getClass().getResource("resource-files/uneven-sections-duplex.pef").toURI());
 		assertEquals(false, p1.containsEightDot());
 		assertEquals(5, p1.getPages());
 		assertEquals(5, p1.getLastPage(1));
@@ -50,7 +25,7 @@ public class PEFBookTest {
 
 	@Test
 	public void testUnevenSectionsSimplex() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, URISyntaxException {
-		PEFBook p1 = PEFBook.load(this.getClass().getResource("resource-files/uneven-sections-simplex.pef").toURI());
+		PEFBook p1 = XPathPEFBook.load(this.getClass().getResource("resource-files/uneven-sections-simplex.pef").toURI());
 		assertEquals(false, p1.containsEightDot());
 		assertEquals(6, p1.getPages());
 		assertEquals(6, p1.getLastPage(1));
@@ -60,7 +35,7 @@ public class PEFBookTest {
 
 	@Test
 	public void testMultipleDimensions() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, URISyntaxException {
-		PEFBook p1 = PEFBook.load(this.getClass().getResource("resource-files/multiple-dimensions.pef").toURI());
+		PEFBook p1 = XPathPEFBook.load(this.getClass().getResource("resource-files/multiple-dimensions.pef").toURI());
 		assertEquals(false, p1.containsEightDot());
 		assertEquals(30, p1.getMaxWidth());
 		assertEquals(20, p1.getMaxHeight());
@@ -69,7 +44,7 @@ public class PEFBookTest {
 
 	@Test
 	public void testEightDot() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, URISyntaxException {
-		PEFBook p1 = PEFBook.load(this.getClass().getResource("resource-files/8-dot-chart.pef").toURI());
+		PEFBook p1 = XPathPEFBook.load(this.getClass().getResource("resource-files/8-dot-chart.pef").toURI());
 		assertEquals(true, p1.containsEightDot());
 	}
 }
