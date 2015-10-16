d1c5f16e587de33c80e90a5c3f14b96f93f025c7
Joel Håkansson <joel.hakansson@mtm.se>
2015-10-16 15:18:17 +0200

    Adds offset and additional search scopes to MarkerReferenceField

--- src/org/daisy/dotify/api/formatter/MarkerReferenceField.java
+++ src/org/daisy/dotify/api/formatter/MarkerReferenceField.java
@@ -1,6 +1,5 @@
 package org.daisy.dotify.api.formatter;
 
-
 /**
  * A MarkerReferenceField is a place holder for a marker's value. 
  * Its value is resolved by the LayoutPerformer when its location 
@@ -16,23 +15,29 @@ public class MarkerReferenceField implements Field {
 	/**
 	 * Defines marker search scopes.
 	 */
-	public static enum MarkerSearchScope {PAGE_CONTENT, PAGE, SEQUENCE}
+	public static enum MarkerSearchScope {PAGE_CONTENT, PAGE, SPREAD, SHEET, SEQUENCE, VOLUME, DOCUMENT}
 
 	private final String markerName;
 	private final MarkerSearchDirection dir;
 	private final MarkerSearchScope scope;
 	private final String textStyle;
+	private final int offset;
 	
 	public MarkerReferenceField(String markerName, MarkerSearchDirection dir, MarkerSearchScope scope) {
 		this(markerName, dir, scope, null);
 	}
-
+	
 	public MarkerReferenceField(String markerName, MarkerSearchDirection dir, MarkerSearchScope scope, String textStyle) {
+		this(markerName, dir, scope, textStyle, 0);
+	}
+
+	public MarkerReferenceField(String markerName, MarkerSearchDirection dir, MarkerSearchScope scope, String textStyle, int offset) {
 		super();
 		this.markerName = markerName;
 		this.dir = dir;
 		this.scope = scope;
 		this.textStyle = textStyle;
+		this.offset = offset;
 	}
 
 	public String getName() {
@@ -50,6 +55,14 @@ public class MarkerReferenceField implements Field {
 	@Override
 	public String getTextStyle() {
 		return textStyle;
+	}
+
+	/**
+	 * Gets the page offset where to start the search
+	 * @return the page offset
+	 */
+	public int getOffset() {
+		return offset;
 	}
 
 }
