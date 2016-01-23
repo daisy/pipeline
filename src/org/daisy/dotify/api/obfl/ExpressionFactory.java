dc23bcd48bb9fff29b2628b93bc03dae7a27ab94
Joel Håkansson <joel.hakansson@mtm.se>
2016-01-23 13:15:09 +0100

    Adds setCreatedWithSPI to all factory apis (factory makers still use the methods deprecated in this commit)

--- src/org/daisy/dotify/api/obfl/ExpressionFactory.java
+++ src/org/daisy/dotify/api/obfl/ExpressionFactory.java
@@ -6,6 +6,29 @@ public interface ExpressionFactory {
 
 	public Expression newExpression();
 
+	/**
+	 * @param itf
+	 * @deprecated use setCreatedWithSPI
+	 */
+	@Deprecated
 	public void setInteger2TextFactory(Integer2TextFactoryMakerService itf);
+	/**
+	 * <p>Informs the implementation that it was discovered and instantiated using
+	 * information collected from a file within the <tt>META-INF/services</tt> directory.
+	 * In other words, it was created using SPI (service provider interfaces).</p>
+	 * 
+	 * <p>This information, in turn, enables the implementation to use the same mechanism
+	 * to set dependencies as needed.</p>
+	 * 
+	 * <p>If this information is <strong>not</strong> given, an implementation
+	 * should avoid using SPIs and instead use
+	 * <a href="http://wiki.osgi.org/wiki/Declarative_Services">declarative services</a>
+	 * for dependency injection as specified by OSGi. Note that this also applies to
+	 * several newInstance() methods in the Java API.</p>
+	 * 
+	 * <p>The class that created an instance with SPI must call this method before
+	 * putting it to use.</p>
+	 */
+	public void setCreatedWithSPI();
 
 }
