061532a5342bb55900ee2b28e956fe056bc6d6c4
Joel Håkansson <joel.hakansson@mtm.se>
2016-05-27 08:45:49 +0200

    Adds a safeguard against unintentional releases uploading

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#version: 2016-05-27
 
 branch=$TRAVIS_BRANCH
 if [ -z "$branch" ]; then
@@ -10,9 +10,31 @@ pullrequest=$TRAVIS_PULL_REQUEST
 if [ -z "$pullrequest" ]; then
 	pullrequest="false"
 fi
+
+version=`grep 'version='  gradle.properties | grep -E -o '[0-9\.]+.+$'`
+if [[ $version == *SNAPSHOT ]]; then
+	is_release=false
+else
+	is_release=true
+fi
+
+if [ `git diff --name-only HEAD HEAD~ | grep gradle.properties -c` = 1 ]; then	
+	prop_change="true"
+else 
+	prop_change="false"
+fi
 echo "Branch: $branch"
 echo "Revision: $revision"
 echo "Pull request: $pullrequest"
+echo "Is release: $is_release"
+echo "Properties changed: $prop_change"
+
+if [ "$prop_change" = "false" ]; then
+	if [ "$is_release" = "true" ]; then
+		echo "No version change since last commit. Is this really a release?"
+		exit 1
+	fi
+fi
 
 if [ "$pullrequest" = "false" ]; then
 	if [ $branch = "master" ]; then
