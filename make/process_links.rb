a05ee7ba019567849f7f084a6e8646fcd618eed5
Bert Frees <bertfrees@gmail.com>
2016-08-31 21:19:03 +0200

    "apidoc" links do not need to fall back to "source"

--- make/process_links.rb
+++ make/process_links.rb
@@ -30,33 +30,16 @@ Dir.glob(base_dir + '/**/*.html').each do |f|
     
     # links to source files with special class attribute
     elsif ['userdoc','apidoc','source'].include?(a['class'])
-      
-      # apidoc falls back to source
-      if a['class'] == 'apidoc'
-        query = SPARQL.parse(%Q{
-          BASE <#{page_url}>
-          PREFIX dp2: <http://www.daisy.org/ns/pipeline/>
-          SELECT ?href WHERE {
-            { <#{a['href']}> dp2:doc ?href }
-            UNION
-            { [] dp2:doc ?href ; dp2:id '#{a['href']}' } .
-            ?href a ?class .
-            FILTER ( ?class = dp2:apidoc || ?class = dp2:source )
-          }
-          ORDER BY ?class
-        })
-      else
-        query = SPARQL.parse(%Q{
-          BASE <#{page_url}>
-          PREFIX dp2: <http://www.daisy.org/ns/pipeline/>
-          SELECT ?href WHERE {
-            { <#{a['href']}> dp2:doc ?href }
-            UNION
-            { [] dp2:doc ?href ; dp2:id '#{a['href']}' } .
-            ?href a dp2:#{a['class']} .
-          }
-        })
-      end
+      query = SPARQL.parse(%Q{
+        BASE <#{page_url}>
+        PREFIX dp2: <http://www.daisy.org/ns/pipeline/>
+        SELECT ?href WHERE {
+          { <#{a['href']}> dp2:doc ?href }
+          UNION
+          { [] dp2:doc ?href ; dp2:id '#{a['href']}' } .
+          ?href a dp2:#{a['class']} .
+        }
+      })
       result = query.execute(graph)
       if not result.empty?
         a['href'] = relativize(page_url, result[0]['href']).to_s
