daisy/pipeline

commit 1b8060a898f86f905e415f8a4cf50e17cb0f5853

Adds inline styles to epub 3

src/org/daisy/dotify/impl/input/xml/resource-files/common/xslt-files/html2obfl_layout.xsl
@@ -24,7 +24,7 @@
 
 	<xsl:template match="/">
 		<obfl version="2011-1" hyphenate="{$hyphenate}">
-			<xsl:attribute name="xml:lang" select="/html:html/@xml:lang"/>
+			<xsl:attribute name="xml:lang" select="html:lang(/html:html)"/>
 			<xsl:call-template name="insertMetadata"/>
 			<xsl:call-template name="insertLayoutMaster"/>
 			<xsl:call-template name="insertProcessorRenderer"/>
@@ -272,6 +272,75 @@
 				<xsl:attribute name="first-line-indent">3</xsl:attribute>
 				<xsl:attribute name="text-indent">3</xsl:attribute>
 				<xsl:attribute name="block-indent">3</xsl:attribute>
+			</xsl:otherwise>
+		</xsl:choose>
+	</xsl:template>
+	
+	<xsl:template match="text()">
+		<xsl:choose>
+			<!-- cases NOT handled by applyStyle/applyFlatStyle -->
+			<xsl:when test="
+				ancestor::*[@xml:lang or @lang][1][not(.=/*)]
+				and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)
+				and not(count(parent::node())=1 and (parent::html:sub or parent::html:sup))">
+					<span><xsl:attribute name="xml:lang"><xsl:value-of select="ancestor::*[@xml:lang or @lang][1]/(@xml:lang, @lang)[1]"/></xsl:attribute><xsl:value-of select="."/></span>
+			</xsl:when>
+			<xsl:otherwise><xsl:value-of select="."/></xsl:otherwise>
+		</xsl:choose>
+	</xsl:template>
+	
+	<xsl:template match="html:em | html:strong" mode="inline-mode">
+		<xsl:call-template name="applyStyle"/>
+	</xsl:template>
+	
+	<xsl:template match="html:i" mode="inline-mode">
+		<xsl:call-template name="applyStyle">
+			<xsl:with-param name="name" select="'em'"/>
+		</xsl:call-template>
+	</xsl:template>
+
+	<xsl:template match="html:b" mode="inline-mode">
+		<xsl:call-template name="applyStyle">
+			<xsl:with-param name="name" select="'strong'"/>
+		</xsl:call-template>
+	</xsl:template>
+
+	<xsl:template match="html:sub | html:sup" mode="inline-mode">
+		<xsl:call-template name="applyFlatStyle"/>
+	</xsl:template>
+	
+	<xsl:template name="applyStyle">
+		<xsl:param name="name" select="name()" as="xs:string"/>
+		<xsl:choose>
+			<xsl:when test="count(node())=0">
+				<xsl:text> </xsl:text>
+			</xsl:when>
+			<xsl:otherwise>
+				<xsl:choose>
+					<xsl:when test="ancestor-or-self::*[html:has-lang(.)][1][not(.=/*)] and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)">
+						<span><xsl:attribute name="xml:lang"><xsl:value-of select="html:lang(.)"/></xsl:attribute><style name="{$name}"><xsl:apply-templates/></style></span>
+					</xsl:when>
+					<xsl:otherwise><style name="{$name}"><xsl:apply-templates/></style></xsl:otherwise>
+				</xsl:choose>
+			</xsl:otherwise>
+		</xsl:choose>
+	</xsl:template>
+	
+	<xsl:template name="applyFlatStyle">
+		<xsl:choose>
+			<!-- text contains a single string -->
+			<xsl:when test="count(node())=1 and text()">
+				<xsl:choose>
+					<xsl:when test="ancestor-or-self::*[html:has-lang(.)][1][not(.=/*)] and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)">
+						<span><xsl:attribute name="xml:lang"><xsl:value-of select="html:lang(.)"/></xsl:attribute><style name="{name()}"><xsl:apply-templates/></style></span>
+					</xsl:when>
+					<xsl:otherwise><style name="{name()}"><xsl:apply-templates/></style></xsl:otherwise>
+				</xsl:choose>
+			</xsl:when>
+			<!-- Otherwise -->
+			<xsl:otherwise>
+				<xsl:message terminate="no">Error: sub/sub contains a complex expression for which there is no specified formatting.</xsl:message>
+				<xsl:apply-templates/>
 			</xsl:otherwise>
 		</xsl:choose>
 	</xsl:template>

src/org/daisy/dotify/impl/input/xml/resource-files/common/xslt-files/html2obfl_base.xsl
@@ -133,6 +133,16 @@
 		<xsl:param name="node" as="element()"/>
 		<xsl:sequence select="tokenize($node/@class, '\s+')" />
 	</xsl:function>
+	
+	<xsl:function name="html:has-lang" as="xs:boolean">
+		<xsl:param name="node" as="element()"/>
+		<xsl:value-of select="not(empty($node[@xml:lang or @lang]))"/>
+	</xsl:function>
+
+	<xsl:function name="html:lang" as="xs:string">
+		<xsl:param name="node" as="element()"/>
+		<xsl:value-of select="$node/ancestor-or-self::*[@xml:lang or @lang][1]/(@xml:lang, @lang)[1]"/>
+	</xsl:function>
 
 	<xsl:function name="epub:noteref" as="xs:boolean">
 		<xsl:param name="node" as="element()"/>

test/org/daisy/dotify/impl/input/xml/XMLInputManagerTest.java
@@ -35,5 +35,23 @@ public class XMLInputManagerTest {
 				new FileInputStream(f),
 				TestHelper.getObflTestContext("resource-files/obfl-filter-sequence.xsl"));
 	}
+	
+	@Test
+	public void testEm_01() throws IOException, TaskSystemException, URISyntaxException, ParserConfigurationException, SAXException, TransformerConfigurationException, TransformerException, TransformerFactoryConfigurationError, XPathExpressionException {
+		File f = TestHelper.toObfl("resource-files/em-input.xhtml", false);
+		TestHelper.assertEqualPath(
+				this.getClass().getResourceAsStream("resource-files/em-expected.xml"),
+				new FileInputStream(f),
+				TestHelper.getObflTestContext("resource-files/obfl-filter-sequence.xsl"));
+	}
+	
+	@Test
+	public void testSub_01() throws IOException, TaskSystemException, URISyntaxException, ParserConfigurationException, SAXException, TransformerConfigurationException, TransformerException, TransformerFactoryConfigurationError, XPathExpressionException {
+		File f = TestHelper.toObfl("resource-files/sub-input.xhtml", false);
+		TestHelper.assertEqualPath(
+				this.getClass().getResourceAsStream("resource-files/sub-expected.xml"),
+				new FileInputStream(f),
+				TestHelper.getObflTestContext("resource-files/obfl-filter-sequence.xsl"));
+	}
 
 }

test/org/daisy/dotify/impl/input/xml/resource-files/em-expected.xml
@@ -1,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><obfl xmlns="http://www.daisy.org/ns/2011/obfl" hyphenate="true" version="2011-1" xml:lang="sv">
+<sequence initial-page-number="1" master="main"><block><span xml:lang="sv"><style name="em">A</style></span></block><block first-line-indent="2"><span xml:lang="en"><style name="em">B</style></span></block><block first-line-indent="2"><style name="em">C</style></block><block first-line-indent="2"><span xml:lang="sv"><style name="em">A</style></span></block><block first-line-indent="2"><span xml:lang="en"><style name="em">B</style></span></block><block first-line-indent="2"><style name="em">C</style></block><block first-line-indent="2"><span xml:lang="sv"><style name="em">A</style></span></block><block first-line-indent="2"><span xml:lang="en"><style name="em">B</style></span></block><block first-line-indent="2"><style name="em">C</style></block><block first-line-indent="2"><span xml:lang="sv"><style name="em">A</style></span></block><block first-line-indent="2"><span xml:lang="en"><style name="em">B</style></span></block><block first-line-indent="2"><style name="em">C</style></block></sequence>
+</obfl>

test/org/daisy/dotify/impl/input/xml/resource-files/em-input.xhtml
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
+    <head>
+        <title>Em/lang</title>
+    </head>
+    <body>
+    	<p xml:lang="sv"><em>A</em></p>
+    	<p xml:lang="en"><em>B</em></p>
+    	<p><em>C</em></p>
+    	<p lang="sv"><em>A</em></p>
+    	<p lang="en"><em>B</em></p>
+    	<p><i>C</i></p>
+    	<p><em xml:lang="sv">A</em></p>
+    	<p><em xml:lang="en">B</em></p>
+    	<p><em>C</em></p>
+    	<p><em lang="sv">A</em></p>
+    	<p><em lang="en">B</em></p>
+    	<p><em>C</em></p>
+    </body>
+</html>

test/org/daisy/dotify/impl/input/xml/resource-files/sub-expected.xml
@@ -1,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><obfl xmlns="http://www.daisy.org/ns/2011/obfl" hyphenate="true" version="2011-1" xml:lang="">
+<sequence initial-page-number="1" master="main"><block>A<style name="sup">2</style></block></sequence>
+</obfl>

test/org/daisy/dotify/impl/input/xml/resource-files/sub-input.xhtml
@@ -1,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Sub/sup combination</title>
+    </head>
+    <body>
+    	<p><sub>A<sup>2</sup></sub></p>
+    </body>
+</html>

commit de1278581c158ff318e6d56bf3d5b1b570ef1862

Fixes a performance problem introduced in commit 1b8060a89

src/org/daisy/dotify/impl/input/xml/resource-files/common/xslt-files/html2obfl_layout.xsl
@@ -361,7 +361,7 @@
 		<xsl:choose>
 			<!-- cases NOT handled by applyStyle/applyFlatStyle -->
 			<xsl:when test="
-				ancestor::*[@xml:lang or @lang][1][not(.=/*)]
+				ancestor::*[@xml:lang or @lang][1][not(self::html:html)]
 				and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)
 				and not(count(parent::node())=1 and (parent::html:sub or parent::html:sup))">
 					<span><xsl:attribute name="xml:lang"><xsl:value-of select="ancestor::*[@xml:lang or @lang][1]/(@xml:lang, @lang)[1]"/></xsl:attribute><xsl:value-of select="."/></span>
@@ -398,7 +398,7 @@
 			</xsl:when>
 			<xsl:otherwise>
 				<xsl:choose>
-					<xsl:when test="ancestor-or-self::*[html:has-lang(.)][1][not(.=/*)] and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)">
+					<xsl:when test="ancestor-or-self::*[html:has-lang(.)][1][not(self::html:html)] and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)">
 						<span><xsl:attribute name="xml:lang"><xsl:value-of select="html:lang(.)"/></xsl:attribute><style name="{$name}"><xsl:apply-templates/></style></span>
 					</xsl:when>
 					<xsl:otherwise><style name="{$name}"><xsl:apply-templates/></style></xsl:otherwise>
@@ -412,7 +412,7 @@
 			<!-- text contains a single string -->
 			<xsl:when test="count(node())=1 and text()">
 				<xsl:choose>
-					<xsl:when test="ancestor-or-self::*[html:has-lang(.)][1][not(.=/*)] and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)">
+					<xsl:when test="ancestor-or-self::*[html:has-lang(.)][1][not(self::html:html)] and not(ancestor::html:em or ancestor::html:strong or ancestor::html:i or ancestor::html:b)">
 						<span><xsl:attribute name="xml:lang"><xsl:value-of select="html:lang(.)"/></xsl:attribute><style name="{name()}"><xsl:apply-templates/></style></span>
 					</xsl:when>
 					<xsl:otherwise><style name="{name()}"><xsl:apply-templates/></style></xsl:otherwise>