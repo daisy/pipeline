f942afcd7a35901a722e6fbadc61cb5eb00e39bc
Bert Frees <bertfrees@gmail.com>
2016-10-28 00:03:36 +0200

    [gui] Support bullet lists in option descriptions
    Also add blank lines after paragraphs, code blocks and lists.

--- gui/src/main/java/org/daisy/pipeline/gui/utils/MarkdownToJavafx.java
+++ gui/src/main/java/org/daisy/pipeline/gui/utils/MarkdownToJavafx.java
@@ -113,8 +113,10 @@ public class MarkdownToJavafx implements Visitor {
         }
 
         @Override
-        public void visit(BulletListNode arg0) {
-                // TODO Auto-generated method stub
+        public void visit(BulletListNode node) {
+                visitChildren(node);
+                Text newline = new Text("\n");
+                this.parent.addChild(newline);
 
         }
 
@@ -182,8 +184,14 @@ public class MarkdownToJavafx implements Visitor {
         }
 
         @Override
-        public void visit(ListItemNode arg0) {
-                // TODO Auto-generated method stub
+        public void visit(ListItemNode node) {
+                for (Node child : node.getChildren()) {
+                        Text bullet = new Text("\u2022 ");
+                        this.parent.addChild(bullet);
+                        child.accept(this);
+                        Text newline = new Text("\n");
+                        this.parent.addChild(newline);
+                }
 
         }
 
@@ -202,7 +210,7 @@ public class MarkdownToJavafx implements Visitor {
         @Override
         public void visit(ParaNode p) {
                 visitChildren(p);
-                Text newline = new Text("\n");
+                Text newline = new Text("\n\n");
                 this.parent.addChild(newline);
 
         }
@@ -319,7 +327,9 @@ public class MarkdownToJavafx implements Visitor {
         public void visit(VerbatimNode verbatim) {
                 Text elem =new Text(verbatim.getText());
                 elem.getStyleClass().add("code");
+                Text newline = new Text("\n");
                 this.parent.addChild(elem);
+                this.parent.addChild(newline);
 
         }
 
