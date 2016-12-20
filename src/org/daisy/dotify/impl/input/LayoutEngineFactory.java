daisy/pipeline

commit 1ebb6cf9a77375b0c1d43b40e9c787bd2e9e2f73

Uses task-api 2.5.0

build.gradle
@@ -53,7 +53,7 @@ sourceSets.main {
 dependencies {
 	provided group: "biz.aQute.bnd", name: "annotation", version: "2.3.0"
 	compile 'org.daisy.dotify:dotify.api:2.10.0'
-	compile 'org.daisy.dotify:dotify.task-api:2.4.0'
+	compile 'org.daisy.dotify:dotify.task-api:2.5.0'
 	compile 'org.daisy.dotify:dotify.common:2.2.0'
 	compile group: 'org.daisy.libs', name: 'jing', version: '20120724.0.0'	
 	compile (group: 'org.daisy.libs', name: 'saxon-he', version: '9.5.1.5') {

src/org/daisy/dotify/impl/input/LayoutEngineFactory.java
@@ -51,6 +51,11 @@ public class LayoutEngineFactory implements TaskGroupFactory {
 	}
 	
 	@Override
+	public boolean supportsSpecification(TaskGroupInformation spec) {
+		return listAll().contains(spec);
+	}
+	
+	@Override
 	@Deprecated
 	public Set<TaskGroupSpecification> listSupportedSpecifications() {
 		return Collections.unmodifiableSet(supportedSpecifications);

src/org/daisy/dotify/impl/input/epub/Epub3InputManagerFactory.java
@@ -46,6 +46,11 @@ public class Epub3InputManagerFactory implements TaskGroupFactory {
 		}
 		return false;
 	}
+	
+	@Override
+	public boolean supportsSpecification(TaskGroupInformation spec) {
+		return listAll().contains(spec);
+	}
 
 	@Override
 	public TaskGroup newTaskGroup(TaskGroupSpecification spec) {

src/org/daisy/dotify/impl/input/text/TextInputManagerFactory.java
@@ -44,6 +44,11 @@ public class TextInputManagerFactory implements TaskGroupFactory {
 		}
 		return false;
 	}
+	
+	@Override
+	public boolean supportsSpecification(TaskGroupInformation spec) {
+		return listAll().contains(spec);
+	}
 
 	@Override
 	public TaskGroup newTaskGroup(TaskGroupSpecification spec) {

src/org/daisy/dotify/impl/input/xml/XMLInputManagerFactory.java
@@ -65,6 +65,11 @@ public class XMLInputManagerFactory implements TaskGroupFactory {
 	}
 	
 	@Override
+	public boolean supportsSpecification(TaskGroupInformation spec) {
+		return listAll().contains(spec);
+	}
+	
+	@Override
 	@Deprecated
 	public Set<TaskGroupSpecification> listSupportedSpecifications() {
 		return Collections.unmodifiableSet(supportedSpecifications);

src/org/daisy/dotify/impl/system/common/DotifyTaskSystem.java
@@ -67,7 +67,6 @@ public class DotifyTaskSystem implements TaskSystem {
 
 		logger.info("Finding path...");
 		for (TaskGroupInformation spec : getPath(imf, TaskGroupInformation.newConvertBuilder(inputFormat, outputFormat).build(), context)) {
-			TaskGroup g = imf.newTaskGroup(spec.toSpecificationBuilder(context).build());
 			if (spec.getActivity()==TaskGroupActivity.ENHANCE) {
 				// For enhance, only include the options required to enable the task group. Once enabled,
 				// additional options may be presented
@@ -76,6 +75,7 @@ public class DotifyTaskSystem implements TaskSystem {
 				}
 			}
 			if (spec.getActivity()==TaskGroupActivity.CONVERT || matchesRequiredOptions(spec, pa, false)) {
+				TaskGroup g = imf.newTaskGroup(spec, context);
 				//TODO: these options should be on the group level instead of on the system level
 				List<TaskOption> opts = g.getOptions();
 				if (opts!=null) {

commit afd7b2604c47b0047accee5190543404d520e94e

Fixes a problem introduced in commit 1ebb6cf9a

build.gradle
@@ -39,7 +39,8 @@ javadoc {
 
 repositories {
 	mavenCentral()
-	mavenLocal()
+	//mavenLocal()
+	maven { url "https://oss.sonatype.org/content/groups/staging" }
 	//maven { url "https://oss.sonatype.org/content/repositories/snapshots" }
 }
 
@@ -53,7 +54,7 @@ sourceSets.main {
 dependencies {
 	provided group: "biz.aQute.bnd", name: "annotation", version: "2.3.0"
 	compile 'org.daisy.dotify:dotify.api:2.10.0'
-	compile 'org.daisy.dotify:dotify.task-api:2.5.0'
+	compile 'org.daisy.dotify:dotify.task-api:2.5.1'
 	compile 'org.daisy.dotify:dotify.common:2.2.0'
 	compile group: 'org.daisy.libs', name: 'jing', version: '20120724.0.0'	
 	compile (group: 'org.daisy.libs', name: 'saxon-he', version: '9.5.1.5') {

src/org/daisy/dotify/impl/input/LayoutEngineFactory.java
@@ -78,7 +78,7 @@ public class LayoutEngineFactory implements TaskGroupFactory {
 		Set<TaskGroupInformation> ret = new HashSet<>();
 		for (TaskGroupInformation info : listAll()) {
 			if (info.matchesLocale(locale)) {
-				ret.add(info.newCopyBuilder().locale(locale).build());
+				ret.add(info);
 			}
 		}
 		return ret;

src/org/daisy/dotify/impl/input/epub/Epub3InputManagerFactory.java
@@ -68,7 +68,7 @@ public class Epub3InputManagerFactory implements TaskGroupFactory {
 		Set<TaskGroupInformation> ret = new HashSet<>();
 		for (TaskGroupInformation info : listAll()) {
 			if (info.matchesLocale(locale)) {
-				ret.add(info.newCopyBuilder().locale(locale).build());
+				ret.add(info);
 			}
 		}
 		return ret;

src/org/daisy/dotify/impl/input/text/TextInputManagerFactory.java
@@ -73,7 +73,7 @@ public class TextInputManagerFactory implements TaskGroupFactory {
 		Set<TaskGroupInformation> ret = new HashSet<>();
 		for (TaskGroupInformation info : listAll()) {
 			if (info.matchesLocale(locale)) {
-				ret.add(info.newCopyBuilder().locale(locale).build());
+				ret.add(info);
 			}
 		}
 		return ret;

src/org/daisy/dotify/impl/input/xml/XMLInputManagerFactory.java
@@ -87,7 +87,7 @@ public class XMLInputManagerFactory implements TaskGroupFactory {
 		Set<TaskGroupInformation> ret = new HashSet<>();
 		for (TaskGroupInformation info : listAll()) {
 			if (info.matchesLocale(locale)) {
-				ret.add(info.newCopyBuilder().locale(locale).build());
+				ret.add(info);
 			}
 		}
 		return ret;