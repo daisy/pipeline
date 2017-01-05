b9be7e1de782ac9cbb3d6e4921b5f8b16a67a722
Bert Frees <bertfrees@gmail.com>
2017-01-05 17:40:35 +0100

    [scripts] Add test for dtbook-to-html

---
+++ modules/scripts/dtbook-to-html/src/test/java/XProcSpecTest.java
@@ -0,0 +1,16 @@
+import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest;
+
+public class XProcSpecTest extends AbstractXSpecAndXProcSpecTest {
+	
+	@Override
+	protected String[] testDependencies() {
+		return new String[] {
+			pipelineModule("common-utils"),
+			pipelineModule("dtbook-to-zedai"),
+			pipelineModule("dtbook-utils"),
+			pipelineModule("fileset-utils"),
+			pipelineModule("file-utils"),
+			pipelineModule("zedai-to-html")
+		};
+	}
+}
