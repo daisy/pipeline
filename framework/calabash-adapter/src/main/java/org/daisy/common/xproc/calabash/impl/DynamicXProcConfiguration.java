831651bbc5d2c0afdd693c701befaa88939246e9
Bert Frees <bertfrees@gmail.com>
2017-06-21 13:34:55 +0200

    git subrepo pull framework
    subrepo:
      subdir:   "framework"
      merged:   "a3bc6e1cd6"
    upstream:
      origin:   "git@github.com:daisy/pipeline-framework.git"
      branch:   "master"
      commit:   "9dc486b912"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

--- framework/calabash-adapter/src/main/java/org/daisy/common/xproc/calabash/impl/DynamicXProcConfiguration.java
+++ framework/calabash-adapter/src/main/java/org/daisy/common/xproc/calabash/impl/DynamicXProcConfiguration.java
@@ -1,5 +1,9 @@
 package org.daisy.common.xproc.calabash.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.daisy.common.xproc.calabash.XProcStepRegistry;
 
 import net.sf.saxon.s9api.Processor;
@@ -9,7 +13,7 @@ import com.xmlcalabash.core.XProcConfiguration;
 import com.xmlcalabash.core.XProcRuntime;
 import com.xmlcalabash.core.XProcStep;
 import com.xmlcalabash.runtime.XAtomicStep;
-
+import com.xmlcalabash.util.Input;
 
 /**
  * This class allows to add new steps dynamically to calabash.
@@ -19,13 +23,25 @@ public class DynamicXProcConfiguration extends XProcConfiguration {
 	/** The step registry. */
 	XProcStepRegistry stepRegistry;
 
+	// see http://www.saxonica.com/documentation9.5/configuration/configuration%2Dfile
+	final static Input saxonCfg = new Input(null, null, Input.Type.XML) {
+		@Override
+		public InputStream getInputStream() {
+			return new ByteArrayInputStream((
+				"<configuration xmlns='http://saxon.sf.net/ns/configuration'>" +
+				"   <global expandAttributeDefaults='false'/>" +
+				"</configuration>\n"
+				).getBytes(StandardCharsets.UTF_8));
+		}
+	};
+
 	/**
 	 * Instantiates a new dynamic x proc configuration which holds the given step registry.
 	 *
 	 * @param stepRegistry the step registry
 	 */
 	public DynamicXProcConfiguration(XProcStepRegistry stepRegistry) {
-		super();
+		super(saxonCfg);
 		this.stepRegistry = stepRegistry;
 	}
 
@@ -37,6 +53,7 @@ public class DynamicXProcConfiguration extends XProcConfiguration {
 	 */
 	public DynamicXProcConfiguration(boolean schemaAware,
 			XProcStepRegistry stepRegistry) {
+		// FIXME: also use saxonCfg?
 		super(schemaAware);
 		this.stepRegistry = stepRegistry;
 	}
