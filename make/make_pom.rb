b307368e3c135dc89f34175b7322631e4647bb1d
Bert Frees <bertfrees@gmail.com>
2017-06-27 10:36:46 +0200

    Make suitable for better integration in super project

--- make/make_pom.rb
+++ make/make_pom.rb
@@ -10,26 +10,45 @@ $stdout << CommaParty.markup(
    [:modelVersion, '4.0.0'],
    [:groupId, 'x'],
    [:artifactId, 'x'],
-   [:version, 'x'],
+   [:version, 'x-SNAPSHOT'],
    [:packaging, 'pom'],
    [:dependencyManagement,
     [:dependencies,
-      [:dependency,
-        [:groupId, 'org.daisy.pipeline'],
-        [:artifactId, 'assembly'],
-        [:version, versions['assembly']],
-        [:type, 'pom'],
-        [:scope, 'import']]]],
-   if modules
-     [:dependencies,
-      modules.map {|mod|
-        group = mod['group']
-        artifact = mod['artifact']
-        version = mod['version']
-        [:dependency,
-         [:groupId, group],
-         [:artifactId, artifact],
-         version && [:version, version]]}]
-   else
-     []
-   end ])
+     [:dependency,
+      [:groupId, 'org.daisy.pipeline'],
+      [:artifactId, 'assembly'],
+      [:version, versions['assembly']],
+      [:type, 'pom'],
+      [:scope, 'import']]]],
+   [:build,
+    [:plugins,
+     [:plugin,
+      [:artifactId, 'maven-dependency-plugin'],
+      [:executions,
+       [:execution,
+        [:id, 'unpack-doc'],
+        [:goals,
+         [:goal, 'unpack']],
+        [:configuration,
+         [:excludes, 'META-INF,META-INF/**/*'],
+         if modules
+           [:artifactItems,
+            modules.map {|mod|
+              group = mod['group']
+              artifact = mod['artifact']
+              version = mod['version']
+              if not (group == 'org.daisy.pipeline.modules' or group.start_with?('org.daisy.pipeline.modules.'))
+                STDERR.puts 'unexpected groupId: ' + group
+                exit 1
+              end
+              [:artifactItem,
+               [:groupId, group],
+               [:artifactId, artifact],
+               version && [:version, version],
+               [:type, 'jar'],
+               [:classifier, 'doc'],
+               [:outputDirectory, (group.gsub('.', '/') + '/' + artifact).sub(/^org\/daisy\/pipeline\//, '')]]}]
+         else
+           []
+         end      
+        ]]]]]]])
