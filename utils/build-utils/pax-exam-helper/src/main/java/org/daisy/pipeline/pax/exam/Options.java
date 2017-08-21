068d9c5389445a2fb4852570d26e04522f6e62d3
Bert Frees <bertfrees@gmail.com>
2017-08-21 15:30:48 +0200

    git subrepo pull utils/build-utils
    subrepo:
      subdir:   "utils/build-utils"
      merged:   "d760794dc6"
    upstream:
      origin:   "git@github.com:daisy/pipeline-build-utils.git"
      branch:   "master"
      commit:   "0368c99294"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

--- utils/build-utils/pax-exam-helper/src/main/java/org/daisy/pipeline/pax/exam/Options.java
+++ utils/build-utils/pax-exam-helper/src/main/java/org/daisy/pipeline/pax/exam/Options.java
@@ -410,6 +410,17 @@ public abstract class Options {
 				// For now don't use "org.ops4j.pax.url.mvn.settings" because Pax Exam itself does not support
 				// system properties inside a settings file.
 				String prop = System.getProperty("org.daisy.org.ops4j.pax.url.mvn.settings");
+				if (prop == null)
+					// Also support "org.ops4j.pax.url.mvn.settings"
+					// Assume this is set only when no system properties are used in the settings file
+					prop = System.getProperty("org.ops4j.pax.url.mvn.settings");
+				// Also support environment variables for more flexibility
+				if (prop == null)
+					prop = System.getenv().get("org.daisy.org.ops4j.pax.url.mvn.settings");
+				if (prop == null) {
+					prop = System.getenv().get("org.ops4j.pax.url.mvn.settings");
+					if (prop != null)
+						System.setProperty("org.ops4j.pax.url.mvn.settings", prop); }
 				if (prop != null)
 					settingsFile = new File(prop);
 				else
