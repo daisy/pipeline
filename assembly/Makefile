b58115f197e229ac192a9ab3d36a2aeff7339f0b
Bert Frees <bertfrees@gmail.com>
2017-10-10 12:16:48 +0200

    [assembly] Fix Docker test
    and add it to the Makefile

--- assembly/Makefile
+++ assembly/Makefile
@@ -1,6 +1,15 @@
+help :
+	echo "make docker:"            >&2
+	echo "	Build a Docker image"  >&2
+	echo "make check-docker:"      >&2
+	echo "	Test the Docker image" >&2
+
 docker :
 	gtar -cz \
 	     target/pipeline2-*_linux \
 	     Dockerfile.without_builder \
 	     --transform='s/Dockerfile.without_builder/Dockerfile/' \
 	| docker build -t daisyorg/pipeline2 -
+
+check-docker :
+	bash src/test/resources/test-docker-image.sh
--- assembly/src/test/resources/test-docker-image.sh
+++ assembly/src/test/resources/test-docker-image.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 
 # This test starts the pipeline inside a Docker container and then
-# starts the dtbook-to-epub script using the cli inside a second
+# starts the dtbook-to-epub3 script using the cli inside a second
 # container
 
-# take any old DTBook file for testing for example from
-# https://github.com/daisy/pipeline-samples
-DOCUMENT=test.xml
-DATA_DIR=$(pwd)
-DATA=$(basename $DOCUMENT).zip
+set -e
+cd $(dirname "$0")
+
+# download test DTBook from https://github.com/daisy/pipeline-samples
+DATA_DIR=../../../target/test-docker/data
+mkdir -p $DATA_DIR
+cd $DATA_DIR
+DTBOOK=hauy_valid.xml
+DATA=$(basename $DTBOOK).zip
+rm -f $DATA
+for f in \
+    $DTBOOK \
+    dtbook.2005.basic.css \
+    valentin.jpg
+do
+    curl https://raw.githubusercontent.com/daisy/pipeline-samples/10bbb8e/dtbook/$f 2>/dev/null >$f
+    zip $DATA $f
+done
+
 CLIENTKEY=clientid
 CLIENTSECRET=sekret
 MOUNT_POINT=/mnt
 
-zip $DATA $DOCUMENT
-
 # run the pipeline
-docker run --name pipeline --rm --detach \
+docker run --name pipeline --detach \
        -e PIPELINE2_WS_HOST=0.0.0.0 \
        -e PIPELINE2_AUTH=true \
        -e PIPELINE2_WS_AUTHENTICATION_KEY=$CLIENTKEY \
@@ -25,7 +37,6 @@ docker run --name pipeline --rm --detach \
 
 # wait for the pipeline to start
 sleep 5
-
 while ! curl localhost:8181/ws/alive >/dev/null 2>/dev/null; do
     echo "Waiting for web service to be up..." >&2
     sleep 2
@@ -34,15 +45,16 @@ done
 # run the cli
 docker run --name cli --rm -it --link pipeline \
        --entrypoint /opt/daisy-pipeline2/cli/dp2 \
-       --volume="$DATA_DIR:$MOUNT_POINT:rw" \
+       --volume="$(pwd):$MOUNT_POINT:rw" \
        daisyorg/pipeline2 \
        --host http://pipeline \
        --starting false \
        --client_key $CLIENTKEY \
        --client_secret $CLIENTSECRET \
-       dtbook-to-epub3 --source $DOCUMENT --output $MOUNT_POINT --data $MOUNT_POINT/$DATA --persistent 
+       dtbook-to-epub3 --source $DTBOOK --output $MOUNT_POINT --data $MOUNT_POINT/$DATA --persistent
 #       help dtbook-to-epub3
-#       dtbook-validator --input-dtbook $DOCUMENT --output $MOUNT_POINT --data $MOUNT_POINT/$DATA
+#       dtbook-validator --input-dtbook $DTBOOK --output $MOUNT_POINT --data $MOUNT_POINT/$DATA
 
 docker stop pipeline
+docker rm pipeline
 
