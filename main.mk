a45ccacc9b973d39368550114a6abcd8883b0523
Bert Frees <bertfrees@gmail.com>
2019-01-28 15:52:20 +0100

    Makefile: some improvements for "make clean"

--- main.mk
+++ main.mk
@@ -53,10 +53,14 @@ $(TARGET_DIR)/maven.mk : $(TARGET_DIR)/maven-modules $(TARGET_DIR)/maven-aggrega
 	echo "MAVEN_MODULES := \$$(shell cat $< 2>/dev/null)" >>$@
 	echo "export MVN_LOCAL_REPOSITORY" >>$@
 	cat $(word 2,$^) | while read -r module; do \
-		echo "aggregators : $$module/pom.xml"; \
+		echo "ifeq (\$$(shell test -e $$module/pom.xml && echo yes), yes)" && \
+		echo "aggregators : $$module/pom.xml" && \
+		echo "endif"; \
 	done >>$@
 	cat $< | while read -r module; do \
-		echo "poms : $$module/pom.xml"; \
+		echo "ifeq (\$$(shell test -e $$module/pom.xml && echo yes), yes)" && \
+		echo "poms : $$module/pom.xml" && \
+		echo "endif"; \
 	done >>$@
 	cat $< | while read -r module; do \
 		pom=$$module/pom.xml; \
@@ -284,10 +288,12 @@ clean-eclipse :
 	rm -rf .metadata
 
 ifeq ($(MAKECMDGOALS), clean)
-include $(shell test -d $(TARGET_DIR)/mk && find $(TARGET_DIR)/mk -name .deps.mk)
+include $(shell for f in $(addsuffix /.deps.mk,$(addprefix $(TARGET_DIR)/mk/,$(MODULES) $(MAVEN_AGGREGATORS))); do \
+                test -e $$f && echo $$f; done)
 else
 ifeq ($(MAKECMDGOALS), clean-eclipse)
-include $(shell test -d $(TARGET_DIR)/mk && find $(TARGET_DIR)/mk -name .deps.mk)
+include $(shell for f in $(addsuffix /.deps.mk,$(addprefix $(TARGET_DIR)/mk/,$(MODULES) $(MAVEN_AGGREGATORS))); do \
+                test -e $$f && echo $$f; done)
 else
 -include $(addsuffix /.deps.mk,$(addprefix $(TARGET_DIR)/mk/,$(MODULES) $(MAVEN_AGGREGATORS)))
 endif
