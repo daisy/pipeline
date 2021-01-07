2e1caceb0b7772d8c2fa500e6bf75f062be677a8
Bert Frees <bertfrees@gmail.com>
2021-01-07 14:11:57 +0100

    [modules] Add XProcSpec test to test the Cereproc adapter with px:ssml-to-audio

---
+++ modules/tts/tts-adapters/tts-adapter-cereproc/src/test/java/XProcSpecTest.java
@@ -0,0 +1,11 @@
+import org.daisy.pipeline.junit.AbstractXSpecAndXProcSpecTest;
+
+public class XProcSpecTest extends AbstractXSpecAndXProcSpecTest {
+
+	@Override
+	protected String[] testDependencies() {
+		return new String[] {
+			pipelineModule("audio-encoder-lame"),
+		};
+	}
+}
