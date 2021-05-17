1242e0896a6bf53c699823a706d8ef7a943f2591
Bert Frees <bertfrees@gmail.com>
2021-05-17 16:34:49 +0200

    Makefile: fix support for rootProject.name in settings.gradle
    fixes commit 573f5e8

--- .make/make-gradle-pom.sh
+++ .make/make-gradle-pom.sh
@@ -6,6 +6,9 @@ for module in "$@"; do
     v=$((cat $module/gradle.properties | grep '^distVersion' ||
          cat $module/gradle.properties | grep '^version' ) | sed 's/.*=//')
     a=$(basename $module)
+    if [ -e $module/settings.gradle ] && cat $module/settings.gradle | grep "rootProject.name" >/dev/null; then
+        a=$(cat $module/settings.gradle | grep '^rootProject.name' | sed "s/^rootProject\.name *= *['\"]\(.*\)['\"]/\1/")
+    fi
     g=$(cat $module/build.gradle | grep '^group' | sed "s/^group *= *['\"]\(.*\)['\"]/\1/")
     echo "  <project xmlns=\"http://maven.apache.org/POM/4.0.0\">"
     echo "    <groupId>$g</groupId>"
