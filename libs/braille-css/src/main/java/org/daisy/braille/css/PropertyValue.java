6f43f89454bccb08f484bb534c5304bbf4feec58
Bert Frees <bertfrees@gmail.com>
2022-11-10 14:11:40 +0100

    [braille-css] Improve usefulness of PropertyValue by implementing Declaration

--- libs/braille-css/src/main/java/org/daisy/braille/css/PropertyValue.java
+++ libs/braille-css/src/main/java/org/daisy/braille/css/PropertyValue.java
@@ -1,29 +1,125 @@
 package org.daisy.braille.css;
 
+import java.util.AbstractList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import cz.vutbr.web.css.CSSProperty;
 import cz.vutbr.web.css.Declaration;
+import cz.vutbr.web.css.Rule;
+import cz.vutbr.web.css.SourceLocator;
 import cz.vutbr.web.css.Term;
+import cz.vutbr.web.css.TermIdent;
+import cz.vutbr.web.csskit.TermIdentImpl;
 
-public class PropertyValue implements Cloneable {
+public class PropertyValue extends AbstractList<Term<?>> implements Cloneable, Declaration {
 	
-	private CSSProperty property;
-	private Term<?> value;
+	private final String propertyName;
+	private final CSSProperty property;
+	private Term<?> value; // not final for clone()
+	private final Term<?> declaration;
+	private final Declaration sourceDeclaration;
 	
-	PropertyValue(CSSProperty property, Term<?> value) {
+	PropertyValue(String propertyName, final CSSProperty property, Term<?> value, Declaration sourceDeclaration) {
+		this.propertyName = propertyName;
 		this.property = property;
 		this.value = value;
+		if (value != null)
+			this.declaration = value;
+		else if (sourceDeclaration.getProperty().equals(propertyName))
+			this.declaration = sourceDeclaration.get(0); // assuming sourceDeclaration is a single TermIdent
+		else
+			// repeater and/or variator was applied
+			this.declaration = new TermIdentImpl() {{
+				value = property.toString();
+				operator = null; }
+					@Override public TermIdent setValue(String value) { throw new UnsupportedOperationException("Unmodifiable"); }
+					@Override public TermIdent setOperator(Operator operator) { throw new UnsupportedOperationException("Unmodifiable"); }
+				};
+		this.sourceDeclaration = sourceDeclaration;
 	}
 	
-	public CSSProperty getProperty() {
+	public CSSProperty getCSSProperty() {
 		return property;
 	}
 	
+	/**
+	 * Returned object is not immutable.
+	 */
 	public Term<?> getValue() {
 		return value;
 	}
+
+	/**
+	 * Returned object can be assumed to be immutable.
+	 */
+	public Declaration getSourceDeclaration() {
+		return sourceDeclaration;
+	}
+	
+	/* ===== Declaration ===== */
+	
+	@Override
+	public String getProperty() {
+		return propertyName;
+	}
+	
+	@Override
+	public boolean isImportant() {
+		return sourceDeclaration.isImportant();
+	}
+	
+	@Override
+	public SourceLocator getSource() {
+		return sourceDeclaration.getSource();
+	}
+	
+	@Override public void setProperty(String p) { throw new UnsupportedOperationException("immutable"); }
+	@Override public void setImportant(boolean i) { throw new UnsupportedOperationException("immutable"); }
+	@Override public void setSource(SourceLocator sl) { throw new UnsupportedOperationException("immutable"); }
+	
+	// List<Term<?>>
+	
+	@Override
+	public int size() {
+		return 1;
+	}
+	
+	@Override
+	public Term<?> get(int i) {
+		return i == 0 ? declaration : null;
+	}
+	
+	// Rule<Term<?>>
+	
+	@Override
+	public List<Term<?>> asList() {
+		return this;
+	}
+	
+	@Override public Rule<Term<?>> replaceAll(List<Term<?>> replacement) { throw new UnsupportedOperationException("immutable"); }
+	@Override public Rule<Term<?>> unlock() { throw new UnsupportedOperationException("immutable"); }
+	
+	// PrettyOutput
+	
+	@Override
+	public String toString(int depth) {
+		return toString();
+	}
+	
+	// Comparable<Declaration>
+	
+	@Override
+	public int compareTo(Declaration o) {
+		if (this.isImportant() && !o.isImportant())
+			return 1;
+		else if (o.isImportant() && !this.isImportant())
+			return -1;
+		return 0;
+	}
+	
+	/* ============================= */
 	
 	@Override
 	public Object clone() {
@@ -32,7 +128,6 @@ public class PropertyValue implements Cloneable {
 				clone = (PropertyValue)super.clone(); }
 			catch (CloneNotSupportedException e) {
 				throw new InternalError("coding error"); }}
-		clone.property = property;
 		if (value != null)
 			clone.value = (Term<?>)value.clone();
 		return clone;
@@ -41,10 +136,12 @@ public class PropertyValue implements Cloneable {
 	@Override
 	public String toString() {
 		if (value != null)
-			return value.toString();
+			return propertyName + ": " + value.toString();
 		else
-			return property.toString();
+			return propertyName + ": " + property.toString();
 	}
+	
+	/* ============================= */
 	
 	private static BrailleCSSDeclarationTransformer transformerInstance
 		= new BrailleCSSDeclarationTransformer(new SupportedBrailleCSS(true, false));
@@ -59,7 +156,7 @@ public class PropertyValue implements Cloneable {
 		Map<String,Term<?>> terms = new HashMap<String,Term<?>>();
 		if (!transformerInstance.parseDeclaration(declaration, properties, terms))
 			return null;
-		String property = declaration.getProperty();
-		return new PropertyValue(properties.get(property), terms.get(property));
+		String propertyName = declaration.getProperty();
+		return new PropertyValue(propertyName, properties.get(propertyName), terms.get(propertyName), declaration);
 	}
 }
