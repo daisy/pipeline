018da6d6b9e6d3f8a6c241e02a6a227dc157915c
Bert Frees <bertfrees@gmail.com>
2022-11-24 00:30:54 +0100

    [braille-css] Allow extending PropertyValue

--- libs/braille-css/src/main/java/org/daisy/braille/css/PropertyValue.java
+++ libs/braille-css/src/main/java/org/daisy/braille/css/PropertyValue.java
@@ -21,13 +21,13 @@ public class PropertyValue extends AbstractList<Term<?>> implements Cloneable, D
 	private final Term<?> declaration;
 	private final Declaration sourceDeclaration;
 	
-	PropertyValue(String propertyName, final CSSProperty property, Term<?> value, Declaration sourceDeclaration) {
+	protected PropertyValue(String propertyName, final CSSProperty property, Term<?> value, Declaration sourceDeclaration) {
 		this.propertyName = propertyName;
 		this.property = property;
 		this.value = value;
 		if (value != null)
 			this.declaration = value;
-		else if (sourceDeclaration.getProperty().equals(propertyName))
+		else if (sourceDeclaration != null && sourceDeclaration.getProperty().equals(propertyName))
 			this.declaration = sourceDeclaration.get(0); // assuming sourceDeclaration is a single TermIdent
 		else
 			// repeater and/or variator was applied
@@ -67,12 +67,12 @@ public class PropertyValue extends AbstractList<Term<?>> implements Cloneable, D
 	
 	@Override
 	public boolean isImportant() {
-		return sourceDeclaration.isImportant();
+		return sourceDeclaration != null ? sourceDeclaration.isImportant() : false;
 	}
 	
 	@Override
 	public SourceLocator getSource() {
-		return sourceDeclaration.getSource();
+		return sourceDeclaration != null ? sourceDeclaration.getSource() : null;
 	}
 	
 	@Override public void setProperty(String p) { throw new UnsupportedOperationException("immutable"); }
