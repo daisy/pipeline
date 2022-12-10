f50c00378af3df07c15202bf0c528dc2fa696c4d
Bert Frees <bertfrees@gmail.com>
2022-12-10 21:56:29 +0100

    git subrepo pull assembly
    subrepo:
      subdir:   "assembly"
      merged:   "ee47a856bb"
    upstream:
      origin:   "git@github.com:daisy/pipeline-assembly.git"
      branch:   "master"
      commit:   "815c3e0c9f"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

--- assembly/src/main/resources/bin/pipeline2.bat
+++ assembly/src/main/resources/bin/pipeline2.bat
@@ -216,7 +216,7 @@ goto :RUN_LOOP
                 rem )
             )
         )
-        set MAIN=org.daisy.pipeline.webservice.impl.PipelineWebService
+        set MAIN=org.daisy.pipeline.webservice.restlet.impl.PipelineWebService
     )
 
     rem Execute the Java Virtual Machine
@@ -224,8 +224,7 @@ goto :RUN_LOOP
 
     rem Logback configuration file
     set SYSTEM_PROPS=%SYSTEM_PROPS% -Dlogback.configurationFile="file:%PIPELINE2_HOME:\=/%/etc/logback.xml"
-    rem Workaround for encoding bugs on Windows
-    set SYSTEM_PROPS=%SYSTEM_PROPS% -Dfile.encoding=UTF8
+    set SYSTEM_PROPS=%SYSTEM_PROPS% -Djava.util.logging.config.file="%PIPELINE2_HOME:\=/%/etc/logging.properties"
     rem to make ${org.daisy.pipeline.data} available in felix.properties (for felix.cache.rootdir)
     if %ENABLE_OSGI% == true (
         set SYSTEM_PROPS=%SYSTEM_PROPS% -Dorg.daisy.pipeline.data="%PIPELINE2_DATA%"
