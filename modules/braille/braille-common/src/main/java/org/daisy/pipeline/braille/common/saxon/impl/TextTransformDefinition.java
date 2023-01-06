fcca26abeedac3f30373d207f35b8a1ca0a57e47
Bert Frees <bertfrees@gmail.com>
2023-01-06 16:13:45 +0100

    [modules] Refactor pf:text-transform to make use of ReflexiveExtensionFunctionProvider

--- modules/braille/braille-common/src/main/java/org/daisy/pipeline/braille/common/saxon/impl/TextTransformDefinition.java
+++ modules/braille/braille-common/src/main/java/org/daisy/pipeline/braille/common/saxon/impl/TextTransformDefinition.java
@@ -1,20 +1,11 @@
 package org.daisy.pipeline.braille.common.saxon.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-import net.sf.saxon.expr.XPathContext;
-import net.sf.saxon.lib.ExtensionFunctionCall;
-import net.sf.saxon.lib.ExtensionFunctionDefinition;
-import net.sf.saxon.om.Item;
-import net.sf.saxon.om.Sequence;
-import net.sf.saxon.om.SequenceIterator;
-import net.sf.saxon.om.StructuredQName;
-import net.sf.saxon.trans.XPathException;
-import net.sf.saxon.value.SequenceExtent;
-import net.sf.saxon.value.SequenceType;
-import net.sf.saxon.value.StringValue;
-
+import org.daisy.common.xpath.saxon.ExtensionFunctionProvider;
+import org.daisy.common.xpath.saxon.ReflexiveExtensionFunctionProvider;
 import org.daisy.pipeline.braille.common.BrailleTranslator;
 import org.daisy.pipeline.braille.common.BrailleTranslator.FromStyledTextToBraille;
 import org.daisy.pipeline.braille.common.BrailleTranslatorRegistry;
@@ -32,15 +23,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component(
-	name = "pf:text-transform",
-	service = { ExtensionFunctionDefinition.class }
+	name = "TextTransform",
+	service = { ExtensionFunctionProvider.class }
 )
-@SuppressWarnings("serial")
-public class TextTransformDefinition extends ExtensionFunctionDefinition {
-	
-	private static final StructuredQName funcname = new StructuredQName("pf",
-			"http://www.daisy.org/ns/pipeline/functions", "text-transform");
-	
+public class TextTransformDefinition extends ReflexiveExtensionFunctionProvider {
+
+	public TextTransformDefinition() {
+		super(TextTransform.class);
+	}
+
+	private BrailleTranslatorRegistry translatorRegistry;
+
 	@Reference(
 		name = "BrailleTranslatorRegistry",
 		unbind = "-",
@@ -52,93 +45,63 @@ public class TextTransformDefinition extends ExtensionFunctionDefinition {
 		translatorRegistry = registry.withContext(logger);
 		logger.debug("Binding BrailleTranslator registry: {}", registry);
 	}
-	
-	private BrailleTranslatorRegistry translatorRegistry;
-	
-	public StructuredQName getFunctionQName() {
-		return funcname;
-	}
-	
-	@Override
-	public int getMinimumNumberOfArguments() {
-		return 2;
-	}
-	
-	@Override
-	public int getMaximumNumberOfArguments() {
-		return 4;
-	}
-	
-	public SequenceType[] getArgumentTypes() {
-		return new SequenceType[] {
-			SequenceType.SINGLE_STRING,
-			SequenceType.ATOMIC_SEQUENCE,
-			SequenceType.ATOMIC_SEQUENCE,
-			SequenceType.ATOMIC_SEQUENCE
-		};
-	}
-	
-	public SequenceType getResultType(SequenceType[] suppliedArgumentTypes) {
-		return SequenceType.ATOMIC_SEQUENCE;
-	}
-	
-	public ExtensionFunctionCall makeCallExpression() {
-		return new ExtensionFunctionCall() {
-			public Sequence call(XPathContext context, Sequence[] arguments) throws XPathException {
-				try {
-					Query query = query(arguments[0].head().getStringValue());
-					List<String> text = sequenceToList(arguments[1]);
-					List<CSSStyledText> styledText = new ArrayList<CSSStyledText>();
-					if (arguments.length > 2) {
-						List<String> style = sequenceToList(arguments[2]);
-						if (style.size() != text.size())
-							throw new XPathException("Lengths of text and style sequences must match");
-						if (arguments.length > 3) {
-							List<String> lang = sequenceToList(arguments[3]);
-							if (lang.size() != text.size())
-								throw new XPathException("Lengths of text and lang sequences must match");
-							for (int i = 0; i < text.size(); i++)
-								styledText.add(new CSSStyledText(text.get(i), style.get(i), parseLocale(lang.get(i)))); }
-						else
-							for (int i = 0; i < text.size(); i++)
-								styledText.add(new CSSStyledText(text.get(i), style.get(i))); }
-					else
-						for (int i = 0; i < text.size(); i++)
-							styledText.add(new CSSStyledText(text.get(i)));
-					for (BrailleTranslator t : translatorRegistry.getWithHyphenator(query)) {
-						FromStyledTextToBraille fsttb;
-						try {
-							fsttb = t.fromStyledTextToBraille(); }
-						catch (UnsupportedOperationException e) {
-							logger.trace("Translator does not implement the FromStyledTextToBraille interface: " + t);
-							continue; }
-						try {
-							// FIXME: don't ignore result style
-							List<StringValue> braille = new ArrayList<>();
-							for (CSSStyledText b : fsttb.transform(styledText))
-								braille.add(new StringValue(b.getText()));
-							return new SequenceExtent(braille); }
-						catch (Exception e) {
-							logger.debug("Failed to translate string with translator " + t);
-							throw e; }
-					}
-					throw new XPathException("Could not find a BrailleTranslator for query: " + query); }
-				catch (XPathException e) {
-					throw e; }
-				catch (Throwable e) {
-					throw new XPathException("Unexpected error in pf:text-transform", e); }
+
+	public class TextTransform {
+
+		public Iterator<String> transform(String query, Iterator<String> text) {
+			return transform(query, text, null);
+		}
+
+		public Iterator<String> transform(String query, Iterator<String> text, Iterator<String> style) {
+			return transform(query, text, style, null);
+		}
+
+		public Iterator<String> transform(String query, Iterator<String> text, Iterator<String> style, Iterator<String> lang) {
+			List<CSSStyledText> styledText = new ArrayList<>();
+			while (text.hasNext()) {
+				String t = text.next();
+				if (style != null) {
+					if (!style.hasNext())
+						throw new IllegalArgumentException("Lengths of text and style sequences must match");
+					if (lang != null) {
+						if (!lang.hasNext())
+							throw new IllegalArgumentException("Lengths of text and lang sequences must match");
+						styledText.add(new CSSStyledText(t, style.next(), parseLocale(lang.next())));
+					} else
+						styledText.add(new CSSStyledText(t, style.next()));
+				} else
+					styledText.add(new CSSStyledText(t));
 			}
-		};
+			if (style != null && style.hasNext())
+				throw new IllegalArgumentException("Lengths of text and style sequences must match");
+			if (lang != null && lang.hasNext())
+				throw new IllegalArgumentException("Lengths of text and lang sequences must match");
+			return transform(query(query), styledText);
+		}
+
+		private Iterator<String> transform(Query query, List<CSSStyledText> styledText) {
+			for (BrailleTranslator t : translatorRegistry.getWithHyphenator(query)) {
+				FromStyledTextToBraille fsttb;
+				try {
+					fsttb = t.fromStyledTextToBraille();
+				} catch (UnsupportedOperationException e) {
+					logger.trace("Translator does not implement the FromStyledTextToBraille interface: " + t);
+					continue;
+				}
+				try {
+					// FIXME: don't ignore result style
+					List<String> braille = new ArrayList<>();
+					for (CSSStyledText b : fsttb.transform(styledText))
+						braille.add(b.getText());
+					return braille.iterator();
+				} catch (Exception e) {
+					logger.debug("Failed to translate string with translator " + t);
+					throw e; }
+			}
+			throw new RuntimeException("Could not find a BrailleTranslator for query: " + query);
+		}
 	}
-	
-	private static List<String> sequenceToList(Sequence seq) throws XPathException {
-		List<String> list = new ArrayList<String>();
-		SequenceIterator iterator = seq.iterate();
-		for (Item item = iterator.next(); item != null; item = iterator.next())
-			list.add(item.getStringValue());
-		return list;
-	}
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(TextTransformDefinition.class);
-	
+
 }
