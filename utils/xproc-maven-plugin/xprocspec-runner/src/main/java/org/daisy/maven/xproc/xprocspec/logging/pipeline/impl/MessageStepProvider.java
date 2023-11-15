94d2a3a417341b949b5fe630ed5c8fe1c3fbbe4b
Bert Frees <bertfrees@gmail.com>
2023-11-15 15:41:02 +0100

    git subrepo pull utils/xproc-maven-plugin
    subrepo:
      subdir:   "utils/xproc-maven-plugin"
      merged:   "ccc5244217"
    upstream:
      origin:   "git@github.com:daisy/xproc-maven-plugin"
      branch:   "master"
      commit:   "aa8c3d9b62"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

--- utils/xproc-maven-plugin/xprocspec-runner/src/main/java/org/daisy/maven/xproc/xprocspec/logging/pipeline/impl/MessageStepProvider.java
+++ utils/xproc-maven-plugin/xprocspec-runner/src/main/java/org/daisy/maven/xproc/xprocspec/logging/pipeline/impl/MessageStepProvider.java
@@ -1,10 +1,13 @@
 package org.daisy.maven.xproc.xprocspec.logging.pipeline.impl;
 
+import java.util.Map;
+
 import com.xmlcalabash.core.XProcRuntime;
 import com.xmlcalabash.runtime.XAtomicStep;
 
 import org.daisy.common.xproc.calabash.XProcStep;
 import org.daisy.common.xproc.calabash.XProcStepProvider;
+import org.daisy.common.xproc.XProcMonitor;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -14,7 +17,7 @@ import org.osgi.service.component.annotations.Component;
 	property = { "type:String={http://www.daisy.org/ns/xprocspec}message" }
 )
 public class MessageStepProvider implements XProcStepProvider {
-	public XProcStep newStep(XProcRuntime runtime, XAtomicStep step) {
+	public XProcStep newStep(XProcRuntime runtime, XAtomicStep step, XProcMonitor monitor, Map<String,String> properties) {
 		return new MessageStep(runtime, step);
 	}
 	private static class MessageStep extends org.daisy.maven.xproc.xprocspec.logging.calabash.impl.MessageStep implements XProcStep {
