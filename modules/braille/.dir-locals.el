fcce76cf1a04901acb24c9f82ae053bd02e853af
Bert Frees <bertfrees@gmail.com>
2024-08-02 10:29:00 +0200

    [modules] Update braille/.dir-locals.el

--- modules/braille/.dir-locals.el
+++ modules/braille/.dir-locals.el
@@ -1,7 +1,4 @@
-((nil
-  . ((compile-command . (format "cd %s && mvn clean install"
-                                (locate-dominating-file buffer-file-name "pom.xml")))))
- (java-mode
+((java-mode
   . ((indent-tabs-mode . t)
      (tab-width . 4)))
  ("NEWS.md"
