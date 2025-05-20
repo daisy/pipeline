134b8f78529d03d650ad639eb63b7658e7b6193e
Nicolas Pavie <pavie.nicolas@gmail.com>
2025-05-20 11:41:06 +0200

    [modules] Change default priority to 15
    to align with Azure and Google adapters.

--- modules/tts/tts-adapters/tts-adapter-aws/src/main/java/org/daisy/pipeline/tts/aws/impl/AWSPollyTTSService.java
+++ modules/tts/tts-adapters/tts-adapter-aws/src/main/java/org/daisy/pipeline/tts/aws/impl/AWSPollyTTSService.java
@@ -46,7 +46,7 @@ public class AWSPollyTTSService implements TTSService {
 		true,
 		"Priority of Amazon voices relative to voices of other engines",
 		false,
-		"2");
+		"15");
 
 	@Override
 	public AWSPollyTTSEngine newEngine(Map<String,String> properties)
