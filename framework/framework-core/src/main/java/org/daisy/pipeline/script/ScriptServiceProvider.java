bf9c595c56f6cb76a2f07ae73fa8d00dd68aa869
Bert Frees <bertfrees@gmail.com>
2025-09-18 17:20:46 +0200

    [framework] ScriptsRegistry: allow scripts to depend on other scripts at registration time

--- framework/framework-core/src/main/java/org/daisy/pipeline/script/ScriptServiceProvider.java
+++ framework/framework-core/src/main/java/org/daisy/pipeline/script/ScriptServiceProvider.java
@@ -2,6 +2,10 @@ package org.daisy.pipeline.script;
 
 public interface ScriptServiceProvider {
 
+	/**
+	 * Note that calling {@code ScriptRegistry.getScript()} from this method or
+	 * from the return {@code Iterable}, is supported.
+	 */
 	public Iterable<ScriptService<?>> getScripts();
 
 }
