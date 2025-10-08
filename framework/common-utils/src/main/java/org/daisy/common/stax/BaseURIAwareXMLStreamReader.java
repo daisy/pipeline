00bd19694a5411e0bb0cead689ac178788f414b8
Bert Frees <bertfrees@gmail.com>
2025-10-08 23:52:46 +0200

    [framework] Let BaseURIAwareXMLStreamReader and BaseURIAwareXMLStreamWriter extend AutoCloseable

--- framework/common-utils/src/main/java/org/daisy/common/stax/BaseURIAwareXMLStreamReader.java
+++ framework/common-utils/src/main/java/org/daisy/common/stax/BaseURIAwareXMLStreamReader.java
@@ -5,7 +5,7 @@ import java.net.URI;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-public interface BaseURIAwareXMLStreamReader extends XMLStreamReader {
+public interface BaseURIAwareXMLStreamReader extends XMLStreamReader, AutoCloseable {
 	
 	public URI getBaseURI() throws XMLStreamException;
 	
